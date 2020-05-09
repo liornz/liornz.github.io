f4e69ee0ddb5b94ddd182e80b7e783fadc7c40d1
liornz <nitzanlior1968@gmail.com>
2020-05-09 22:19:48 +0200

    JS Counter - closure functions

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log("Hello");
+function count () {
+    var a = 0;
+    console.log("reset counter - a = 0");
+    return function () {
+        a++;
+        console.log(a);
+    }
+}
+console.log("counter1");
+var counter1 = count();
+counter1 ();
+counter1 ();
+counter1 ();
+
+console.log("counter2");
+var counter2 = count();
+counter2 ();
+
