a39d685fa421bd9dab061a5171ee30e7389a34a6
liornz <nitzanlior1968@gmail.com>
2020-06-03 16:54:08 +0200

    some changes to the navbar

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+      document.getElementById("header").style.top = "30px";
+    } else {
+      document.getElementById("header").style.top = "-100px";
+    }
+  }
