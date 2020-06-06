081cc04fff3e6251d16c0860b41454d884c29497
liornz <nitzanlior1968@gmail.com>
2020-06-06 15:56:50 +0200

    Changes to top menu and addition of video pages
    The top menu is now responsive and with social media icons.
    I added pages for future video pages.

---
+++ video-page.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+      document.getElementById("topnavbar").style.top = "30px";
+    } else {
+      document.getElementById("topnavbar").style.top = "-100px";
+    }
+  }
+  function mobileMenu() {
+    var x = document.getElementById("topnavbar");
+    if (x.className === "topnav") {
+      x.className += " mobile";
+    } else {
+      x.className = "topnav";
+    }
+  }
