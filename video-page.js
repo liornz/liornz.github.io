0df9601e7c470dba389bc9082bf25e67dbd2875e
liornz <nitzanlior1968@gmail.com>
2020-06-07 17:39:08 +0200

    Update video-page.js

--- video-page.js
+++ video-page.js
@@ -1,12 +1,14 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-      document.getElementById("topnavbar").style.top = "30px";
-    } else {
-      document.getElementById("topnavbar").style.top = "-100px";
-    }
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("topnavbar").style.top = "30px";
+  } else {
+    document.getElementById("topnavbar").style.top = "-100px";
   }
+  prevScrollpos = currentScrollPos;
+}
+
   function mobileMenu() {
     var x = document.getElementById("topnavbar");
     if (x.className === "topnav") {
